982a1584b3a3142389a0e6c8d3ee8ede1f0c162f
Sudarshan Magar <magarsudarshan77@gmail.com>
2025-01-16 20:15:38 +0530

    getAlladmin route

---
+++ controllers/getAllAdmin.js
@@ -0,0 +1,22 @@
+const db = require('../config/db');
+const getAllAdmin=async(req,res)=>{
+    try{
+        const [allAdmin]=await db.query(
+            'SELECT * FROM admin'
+            
+        )
+        res.status(200).send({
+            success:true,
+            Data:allAdmin,
+        })
+    }catch(error){
+        res.status(500).send({
+            success:false,
+            msg:'Server Problem.',
+            error:error
+        })
+    }
+    
+}
+
+module.exports=getAllAdmin;
--- routes/society-app-Route.js
+++ routes/society-app-Route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const {SuperAdminLogin} = require("../controllers/society-app");
 const {adminCreate}=require('../controllers/admin-create');
+const getAllAdmin = require("../controllers/getAllAdmin");
 const router = express.Router();
 router.post("/super/login",SuperAdminLogin);
 router.post('/admin/create',adminCreate);
+router.get('/allAdmin',getAllAdmin);
 module.exports = router;
